07b38a55b5705e4573f5781ecffc0266b64f80e0
dynobird <info@dynobird.com>
2021-02-12 13:24:46 +0700

    ✨[FIX] php formater

--- src/laravel/fileMaker.ts
+++ src/laravel/fileMaker.ts
@@ -1,7 +1,9 @@
 import { GenerateConfig } from "../lib/type";
 import fs from "fs"
 import chalk  from "chalk"
-
+import prettier from "prettier"
+// @ts-ignore
+import phpPlugin from "@prettier/plugin-php";
 export class FileMaker {
     async makeMigration(config: GenerateConfig, fileName: string, data: string) {
 
@@ -9,7 +11,12 @@ export class FileMaker {
 
         let filePath = `${thisDirectory}/${config.migrationsDir}/${fileName}.php`
 
-        fs.writeFileSync(filePath, data)
+        let formated = prettier.format(data, {
+            plugins: phpPlugin,
+            parser: "php"
+          });
+
+        fs.writeFileSync(filePath, formated)
         console.log(chalk.green(" Generated  : "+filePath))
     }
 }
